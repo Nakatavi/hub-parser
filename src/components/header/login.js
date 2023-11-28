aa7990413b1e40bc7a911f2a82c88e6bcd982ed4
shevchukIvan <i.shevcuk@apiko.com>
2023-11-28 11:18:08 +0200

    Add login UI

--- src/components/header/login.js
+++ src/components/header/login.js
@@ -1,52 +1,40 @@
-import React, { useState } from 'react';
-import { useAuth } from '../AuthContext';
-import { useNavigate } from 'react-router';
+import React, { useState } from "react";
+import { useAuth } from "../AuthContext";
+import { useNavigate } from "react-router";
+import "../../styles/login.css";
 
 const LoginForm = () => {
   const { login } = useAuth();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    
-    if (username === 'user' && password === 'pas') {
-        console.log('Login button clicked');
-  console.log('Username:', username);
-  console.log('Password:', password);
+    if (username === "mPARmANi" && password === "BuShiMiN") {
+      console.log("Login button clicked");
+      console.log("Username:", username);
+      console.log("Password:", password);
       login();
-      navigate('/');
-      setError('');
+      navigate("/");
+      setError("");
     } else {
-      setError('Invalid username or password');
+      setError("Invalid username or password");
     }
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button onClick={handleLogin}>Login</button>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+    <div className="login">
+    <div class="colm-form">
+        <div class="form-container">
+          <input type="text" placeholder="Username" id="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+          <input type="password" placeholder="Password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+          <button class="btn-login" onClick={handleLogin}>Login</button>
+          {error && <p style={{ color: "red" }}>{error}</p>}
+        </div>
     </div>
+    </div>
+          
   );
 };
 
